2a3f63a0489f28e302f34856f66ef302de9d8565
migreva <mikevee313@gmail.com>
2015-09-04 16:34:56 -0400

    email parsing tests, still need to implement

---
+++ src/server/util/email.js
@@ -0,0 +1,43 @@
+
+let validEmailDomains = [
+  '@michigan.com',
+  '@freep.com',
+  '@lsj.com',
+];
+
+// http://stackoverflow.com/a/46181/1337683
+let emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+/**
+ * Detemins whether an email is valid or not. Tests for valid email format, and
+ * also that the domain is in validEmailDomains
+ *
+ * @param {String} email - Email
+ * @return {Boolean} True if the email is valid, false otherwise
+ */
+function isValidEmail(email) {
+
+  // Reject if the email is formatted incorrectly
+  if (!emailRegex.test(email)) {
+    return false;
+  }
+
+  // Reject if it's not a supported domain
+  let validDomain = false;
+  for (let domain of validEmailDomains) {
+    let regex = RegExp(`${domain}$`);
+
+    if (regex.test(email)) {
+      validDomain = true;
+      break;
+    }
+  }
+
+  return validDomain === true;
+}
+
+module.exports = {
+  validEmailDomains,
+  emailRegex,
+  isValidEmail
+}
---
+++ test/server/email.js
@@ -0,0 +1,53 @@
+import { equal } from 'assert';
+
+import Email from '../../src/server/util/email';
+
+describe('Tests email parsing', function() {
+
+  it('Tests regexing of invalid formatted emails', function() {
+    let invalidEmails = [
+      'this',
+      'that email',
+      'this@is an email',
+      '12345678910',
+      '@@@@@@email',
+      '@gmail.com',
+      'this@@@.com',
+    ];
+
+    for (let email of invalidEmails) {
+      equal(Email.emailRegex.test(email), false, `Email ${email} should not be valid`)
+    }
+  });
+
+  it('Tests regexing valid formatted emails', function() {
+    let validEmails = [
+      'test@test.com',
+      'this@that.com',
+      't@a.co',
+      't123@123.com',
+      '123@123.aw'
+    ];
+
+    for (let email of validEmails) {
+      equal(Email.emailRegex.test(email), true, `Email ${email} should be valid`);
+    }
+  });
+
+  it('Test the validation function', function() {
+    let invalidDomains = [
+      'someemail@gmail.com',
+      'someotheremail@yahoo.com',
+      'someemail@hotmail.com'
+    ];
+
+    for (let email of invalidDomains) {
+      equal(Email.isValidEmail(email), false, `Email ${email} should be invalid`);
+    }
+
+    for (let domain of Email.validEmailDomains) {
+      let testEmail = `test${domain}`;
+      equal(Email.isValidEmail(testEmail), true, `Email ${testEmail} should be valid`);
+    }
+  });
+});
