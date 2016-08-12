d901f9d15ca00a4b250a941305ca325aa271bf3a
Mike Varano <mikevee313@gmail.com>
2016-08-12 12:09:43 -0400

    a bunch of mobile tweaks

--- src/server/util/parse.js
+++ src/server/util/parse.js
@@ -2,14 +2,16 @@
 
 export function formatInviteUrl(token) {
   let endpoint = 'http://localhost:3000';
-  if (process.env.NODE_ENV) endpoint = 'http://breakfast.im';
+  if (process.env.NODE_ENV === 'production') endpoint = 'https://breakfast.im';
+  else if (process.env.NODE_ENV === 'test') endpoint = 'http://test.breakfast.im';
 
   return `${endpoint}/register/${token}/`;
 }
 
 export function formatPasswordResetUrl(token) {
   let endpoint = 'http://localhost:3000';
-  if (process.env.NODE_ENV) endpoint = 'http://breakfast.im';
+  if (process.env.NODE_ENV === 'production') endpoint = 'https://breakfast.im';
+  else if (process.env.NODE_ENV === 'test') endpoint = 'http://test.breakfast.im';
 
   return `${endpoint}/password-reset/${token}/`;
 }
