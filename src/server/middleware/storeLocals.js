472eb184a6f43508d9f14121c0e690be8860f5f1
Mike Varano <mikevee313@gmail.com>
2016-10-06 12:04:43 -0400

    setting up s3 static files

--- src/server/middleware/storeLocals.js
+++ src/server/middleware/storeLocals.js
@@ -1,7 +1,13 @@
+
+import debug from 'debug';
+
+const logger = debug('breakfast:storeLocals');
+
 export default function storeLocals() {
   return function (req, res, next) {
     res.locals.node_env = process.env.NODE_ENV;
     res.locals.user = req.user;
+    res.locals.s3_bucket = process.env.S3_BUCKET || '';
     next();
   };
 }
