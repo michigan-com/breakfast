4e6dad3f4a488ecd38bb9a7e00be7cfe2a223b21
Mike Varano <mikevee313@gmail.com>
2016-10-13 14:05:42 -0400

    gallery tweaks, gallery ready to go

--- src/client/gallery.js
+++ src/client/gallery.js
@@ -154,9 +154,9 @@ class Gallery extends Component {
 }
 
 const SCORE_IMAGES = [
-  'https://michigan-breakfast.s3.amazonaws.com/222c3e5f-f516-44de-9a22-671ebeaff0af.png',
-  'https://michigan-breakfast.s3.amazonaws.com/11b5ae75-5e4f-464e-8283-1d30a4b76c9c.png',
   'https://michigan-breakfast.s3.amazonaws.com/173dbbcf-6252-4990-a93c-a295faef1d22.png',
+  'https://michigan-breakfast.s3.amazonaws.com/222c3e5f-f516-44de-9a22-671ebeaff0af.png',
+  // 'https://michigan-breakfast.s3.amazonaws.com/11b5ae75-5e4f-464e-8283-1d30a4b76c9c.png',
   'https://michigan-breakfast.s3.amazonaws.com/0452d894-159d-4725-85fc-218ed5fe6603.png',
 ];
 
@@ -196,12 +196,13 @@ const LIST_IMAGES = [
   'https://michigan-breakfast.s3.amazonaws.com/098b3a85-7172-4ac0-a4d8-985c2076f366.png',
   'https://michigan-breakfast.s3.amazonaws.com/11a1d15a-c1bb-43b5-85b8-2b311040d709.png',
   'https://michigan-breakfast.s3.amazonaws.com/035715fb-8fc8-4eae-9b01-67ca3e9d752f.png',
+  'https://michigan-breakfast.s3.amazonaws.com/25a2da75-59d2-4c45-8e90-bb11b14581b5.png',
 ];
 
 const BRANDING_IMAGES = [
   'https://michigan-breakfast.s3.amazonaws.com/1fa3900d-2ffa-47e7-8c0a-f1f1af839ca4.png',
   'https://michigan-breakfast.s3.amazonaws.com/057a744b-b1bf-4abb-9bed-fc1e38c03b97.png',
-  'https://michigan-breakfast.s3.amazonaws.com/2adce932-96a9-4155-bf5c-df9d825444fd.png',
+  'https://michigan-breakfast.s3.amazonaws.com/1795207b-8445-469c-8312-501e3f9b5a7d.png',
   'https://michigan-breakfast.s3.amazonaws.com/0c3e5b2e-ef30-428e-8403-e1b2165b2cae.png',
   'https://michigan-breakfast.s3.amazonaws.com/1958cf43-0630-4226-9a79-bf1b4b874b7a.png',
   'https://michigan-breakfast.s3.amazonaws.com/0b61cb36-239d-4c96-bb60-933ec8d877a8.png',
@@ -209,17 +210,16 @@ const BRANDING_IMAGES = [
   'https://michigan-breakfast.s3.amazonaws.com/24e30e56-6558-416c-a7e9-f756f9a48f39.png',
   'https://michigan-breakfast.s3.amazonaws.com/1474629e-700e-4c30-85af-7878f62d3a70.png',
   'https://michigan-breakfast.s3.amazonaws.com/0b6e058f-2a6b-48b5-9b9f-e31ba17a3dcf.png',
-  'https://michigan-breakfast.s3.amazonaws.com/02739c5d-1430-404f-ba96-494e4a11a49f.png',
   'https://michigan-breakfast.s3.amazonaws.com/0537fc42-48ac-46c5-bc27-42affa21b708.png',
   'https://michigan-breakfast.s3.amazonaws.com/084e8520-6aba-4b5c-81b4-c7faae6aa127.png',
   'https://michigan-breakfast.s3.amazonaws.com/228c1434-49e0-43a4-b95b-ff8626d8b93b.png',
   'https://michigan-breakfast.s3.amazonaws.com/28a09d1a-e551-4f79-b719-af5978ffddd3.png',
+  'https://michigan-breakfast.s3.amazonaws.com/02739c5d-1430-404f-ba96-494e4a11a49f.png',
 ];
 
 const INFO_IMAGES = [
   'https://michigan-breakfast.s3.amazonaws.com/184c2d9d-f167-4755-a4e9-f1cb6fde9e64.png',
   'https://michigan-breakfast.s3.amazonaws.com/24ebdb9e-c251-402e-9807-3a218f559a72.png',
-  'https://michigan-breakfast.s3.amazonaws.com/27b80c22-a553-4815-9395-834c07ce2dd9.png',
   'https://michigan-breakfast.s3.amazonaws.com/19c820b7-7c71-4c40-a504-df715e5eb7c0.png',
   'https://michigan-breakfast.s3.amazonaws.com/0078860c-963e-4c37-a1b5-273b79d3f43e.png',
   'https://michigan-breakfast.s3.amazonaws.com/0a1e81db-07ea-4e8b-8f3f-28c59d6303ee.png',
@@ -227,12 +227,12 @@ const INFO_IMAGES = [
   'https://michigan-breakfast.s3.amazonaws.com/234f6207-842e-4bbb-8cf4-c8a5d9c55a6f.png',
   'https://michigan-breakfast.s3.amazonaws.com/299a7c11-f0c4-4ed1-9f98-89a9d56a216c.png',
   'https://michigan-breakfast.s3.amazonaws.com/065fff7b-ecd8-4fa2-86e1-48b893505f29.png',
-  'https://michigan-breakfast.s3.amazonaws.com/21a43f26-db05-4a51-b097-08a6843e9d89.png',
   'https://michigan-breakfast.s3.amazonaws.com/19520df4-ce58-4183-94ac-f394a2e8ade3.png',
-  'https://michigan-breakfast.s3.amazonaws.com/261fa311-3242-47e9-a098-a37600a25a7e.png',
   'https://michigan-breakfast.s3.amazonaws.com/19e24df6-b57e-46fc-b1dc-f1d2a3bae372.png',
   'https://michigan-breakfast.s3.amazonaws.com/1179a136-a19d-44de-ba13-e0488124187e.png',
+  'https://michigan-breakfast.s3.amazonaws.com/21a43f26-db05-4a51-b097-08a6843e9d89.png',
   'https://michigan-breakfast.s3.amazonaws.com/0659757e-7507-4229-b77a-ea0f946f1fbf.png',
+  'https://michigan-breakfast.s3.amazonaws.com/261fa311-3242-47e9-a098-a37600a25a7e.png',
 ];
 
 const init = () => {
