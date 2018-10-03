18eeba25cf1f5d9e62ab86e47796ac4525fcab4b
Mike Varano <mikevee313@gmail.com>
2018-10-03 14:21:20 -0400

    oops no YOU pushed a bug

--- src/client/elections/components/templates/helpers/candidate-info.js
+++ src/client/elections/components/templates/helpers/candidate-info.js
@@ -1,10 +1,10 @@
 
-function hidePartyAbbr(candidate) {
+function shouldHidePartyAbbr(candidate) {
   return candidate.party.abbr === 'hidePartyAbbr' || candidate.party.abbr.toLowerCase() === 'o';
 }
 
 export function getCandidateText(candidate) {
-  var hidePartyAbbr = hidePartyAbbr(candidate);
+  var hidePartyAbbr = shouldHidePartyAbbr(candidate);
   var candidateInfo = `${hidePartyAbbr ? '' : candidate.party.abbr}`
   if (candidate.location)  {
     candidateInfo += hidePartyAbbr ? candidate.location : `-${candidate.location}`;
